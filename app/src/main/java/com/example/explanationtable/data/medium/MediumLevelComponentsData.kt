e0fa56c3da4a32e8f5fe9c615829f1b6c8d064b1
Mahdi Pakravan <mahdipakravan1998@gmail.com>
2025-06-03 10:42:11 +0330

    Add review page data code for medium difficulty levels

---
+++ app/src/main/java/com/example/explanationtable/data/medium/MediumLevelComponentsData.kt
@@ -0,0 +1,709 @@
+package com.example.explanationtable.data.medium
+
+import com.example.explanationtable.model.LevelComponentsTable
+
+/**
+ * A list of LevelComponentsTable that stores different components for each level.
+ */
+val mediumLevelComponentsData: List<LevelComponentsTable> = listOf(
+    LevelComponentsTable(
+        id = 1,
+        components = mapOf(
+            0 to listOf("آل"),
+            1 to listOf("آباء"),
+            2 to listOf("بئر"),
+            3 to listOf("اکمه"),
+            4 to listOf("لائم"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 2,
+        components = mapOf(
+            0 to listOf("حل"),
+            1 to listOf("آثار"),
+            2 to listOf("ازر"),
+            3 to listOf("اربع"),
+            4 to listOf("لازب"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 3,
+        components = mapOf(
+            0 to listOf("مس"),
+            1 to listOf("ابنت"),
+            2 to listOf("ابل"),
+            3 to listOf("اعلم"),
+            4 to listOf("سنبل"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 4,
+        components = mapOf(
+            0 to listOf("جو"),
+            1 to listOf("آصال"),
+            2 to listOf("اصر"),
+            3 to listOf("رقبه"),
+            4 to listOf("واصب"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 5,
+        components = mapOf(
+            0 to listOf("آن"),
+            1 to listOf("جهاد"),
+            2 to listOf("اصم"),
+            3 to listOf("ارحم"),
+            4 to listOf("ناصح"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 6,
+        components = mapOf(
+            0 to listOf("اخ"),
+            1 to listOf("اخزی"),
+            2 to listOf("انف"),
+            3 to listOf("آلهه"),
+            4 to listOf("خزنه"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 7,
+        components = mapOf(
+            0 to listOf("اب"),
+            1 to listOf("اثاث"),
+            2 to listOf("الف"),
+            3 to listOf("اصغر"),
+            4 to listOf("بالغ"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 8,
+        components = mapOf(
+            0 to listOf("جب"),
+            1 to listOf("اظلم"),
+            2 to listOf("باد"),
+            3 to listOf("اطغی"),
+            4 to listOf("بلاغ"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 9,
+        components = mapOf(
+            0 to listOf("رس"),
+            1 to listOf("سحاب"),
+            2 to listOf("بحر"),
+            3 to listOf("سارق"),
+            4 to listOf("ساحر"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 10,
+        components = mapOf(
+            0 to listOf("بر"),
+            1 to listOf("عظام"),
+            2 to listOf("بغی"),
+            3 to listOf("عقبی"),
+            4 to listOf("راغب"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 11,
+        components = mapOf(
+            0 to listOf("بر"),
+            1 to listOf("اتقی"),
+            2 to listOf("آیه"),
+            3 to listOf("تربص"),
+            4 to listOf("رقیب"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 12,
+        components = mapOf(
+            0 to listOf("ام"),
+            1 to listOf("خطبه"),
+            2 to listOf("بشر"),
+            3 to listOf("تبرج"),
+            4 to listOf("مبشر"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 13,
+        components = mapOf(
+            0 to listOf("رق"),
+            1 to listOf("امام"),
+            2 to listOf("بعل"),
+            3 to listOf("اجدر"),
+            4 to listOf("قاعد"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 14,
+        components = mapOf(
+            0 to listOf("قط"),
+            1 to listOf("عمره"),
+            2 to listOf("عین"),
+            3 to listOf("علقه"),
+            4 to listOf("طریق"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 15,
+        components = mapOf(
+            0 to listOf("کل"),
+            1 to listOf("منبث"),
+            2 to listOf("نار"),
+            3 to listOf("مرسل"),
+            4 to listOf("لباس"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 16,
+        components = mapOf(
+            0 to listOf("حر"),
+            1 to listOf("رحمن"),
+            2 to listOf("جان"),
+            3 to listOf("اثنا"),
+            4 to listOf("رمان"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 17,
+        components = mapOf(
+            0 to listOf("حر"),
+            1 to listOf("ابحر"),
+            2 to listOf("بیع"),
+            3 to listOf("ثامن"),
+            4 to listOf("رحیم"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 18,
+        components = mapOf(
+            0 to listOf("حی"),
+            1 to listOf("حکمت"),
+            2 to listOf("بین"),
+            3 to listOf("حسنه"),
+            4 to listOf("یمین"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 19,
+        components = mapOf(
+            0 to listOf("یم"),
+            1 to listOf("شعوب"),
+            2 to listOf("اسم"),
+            3 to listOf("شعیب"),
+            4 to listOf("موسی"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 20,
+        components = mapOf(
+            0 to listOf("ید"),
+            1 to listOf("جمله"),
+            2 to listOf("خیل"),
+            3 to listOf("خالق"),
+            4 to listOf("دلیل"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 21,
+        components = mapOf(
+            0 to listOf("هد"),
+            1 to listOf("جمعه"),
+            2 to listOf("دون"),
+            3 to listOf("میته"),
+            4 to listOf("دعوت"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 22,
+        components = mapOf(
+            0 to listOf("جم"),
+            1 to listOf("مزاج"),
+            2 to listOf("ملح"),
+            3 to listOf("مدکر"),
+            4 to listOf("مالک"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 23,
+        components = mapOf(
+            0 to listOf("بث"),
+            1 to listOf("اعلی"),
+            2 to listOf("دار"),
+            3 to listOf("اکثر"),
+            4 to listOf("ثلاث"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 24,
+        components = mapOf(
+            0 to listOf("آن"),
+            1 to listOf("آذان"),
+            2 to listOf("بصر"),
+            3 to listOf("ابرص"),
+            4 to listOf("ناصر"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 25,
+        components = mapOf(
+            0 to listOf("حب"),
+            1 to listOf("بغال"),
+            2 to listOf("بطن"),
+            3 to listOf("بینه"),
+            4 to listOf("باطن"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 26,
+        components = mapOf(
+            0 to listOf("حب"),
+            1 to listOf("بنات"),
+            2 to listOf("اسف"),
+            3 to listOf("باطل"),
+            4 to listOf("باسط"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 27,
+        components = mapOf(
+            0 to listOf("حظ"),
+            1 to listOf("طلاق"),
+            2 to listOf("علو"),
+            3 to listOf("عامل"),
+            4 to listOf("ظالم"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 28,
+        components = mapOf(
+            0 to listOf("کل"),
+            1 to listOf("مرحب"),
+            2 to listOf("لوح"),
+            3 to listOf("مأمن"),
+            4 to listOf("لحوم"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 29,
+        components = mapOf(
+            0 to listOf("من"),
+            1 to listOf("نعجه"),
+            2 to listOf("موج"),
+            3 to listOf("نعمت"),
+            4 to listOf("نجوم"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 30,
+        components = mapOf(
+            0 to listOf("من"),
+            1 to listOf("ارجل"),
+            2 to listOf("هاد"),
+            3 to listOf("کاتب"),
+            4 to listOf("نجات"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 31,
+        components = mapOf(
+            0 to listOf("دم"),
+            1 to listOf("مذؤم"),
+            2 to listOf("لذت"),
+            3 to listOf("مثنی"),
+            4 to listOf("مؤذن"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 32,
+        components = mapOf(
+            0 to listOf("ذل"),
+            1 to listOf("سامر"),
+            2 to listOf("جزء"),
+            3 to listOf("ادهی"),
+            4 to listOf("لمزه"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 33,
+        components = mapOf(
+            0 to listOf("حج"),
+            1 to listOf("اجنه"),
+            2 to listOf("لوط"),
+            3 to listOf("مبدی"),
+            4 to listOf("جنود"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 34,
+        components = mapOf(
+            0 to listOf("حق"),
+            1 to listOf("مخاض"),
+            2 to listOf("کهف"),
+            3 to listOf("مخرج"),
+            4 to listOf("قاهر"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 35,
+        components = mapOf(
+            0 to listOf("جن"),
+            1 to listOf("نکاح"),
+            2 to listOf("نقض"),
+            3 to listOf("اشهر"),
+            4 to listOf("ناقه"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 36,
+        components = mapOf(
+            0 to listOf("رب"),
+            1 to listOf("آخرت"),
+            2 to listOf("حزب"),
+            3 to listOf("متخذ"),
+            4 to listOf("برزخ"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 37,
+        components = mapOf(
+            0 to listOf("خد"),
+            1 to listOf("تقلب"),
+            2 to listOf("نور"),
+            3 to listOf("ابکم"),
+            4 to listOf("دلوک"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 38,
+        components = mapOf(
+            0 to listOf("سر"),
+            1 to listOf("منزل"),
+            2 to listOf("خال"),
+            3 to listOf("اشقی"),
+            4 to listOf("رزاق"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 39,
+        components = mapOf(
+            0 to listOf("شر"),
+            1 to listOf("رجفه"),
+            2 to listOf("بیض"),
+            3 to listOf("ساعت"),
+            4 to listOf("رفیع"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 40,
+        components = mapOf(
+            0 to listOf("شک"),
+            1 to listOf("احرص"),
+            2 to listOf("حین"),
+            3 to listOf("مسمع"),
+            4 to listOf("کریم"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 41,
+        components = mapOf(
+            0 to listOf("لم"),
+            1 to listOf("شاعر"),
+            2 to listOf("خلد"),
+            3 to listOf("مجمع"),
+            4 to listOf("معلم"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 42,
+        components = mapOf(
+            0 to listOf("رد"),
+            1 to listOf("قرآن"),
+            2 to listOf("حفظ"),
+            3 to listOf("شیعه"),
+            4 to listOf("دافع"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 43,
+        components = mapOf(
+            0 to listOf("سد"),
+            1 to listOf("آفاق"),
+            2 to listOf("ابن"),
+            3 to listOf("اطهر"),
+            4 to listOf("دابه"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 44,
+        components = mapOf(
+            0 to listOf("سم"),
+            1 to listOf("وابل"),
+            2 to listOf("ملت"),
+            3 to listOf("مضغه"),
+            4 to listOf("مبلغ"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 45,
+        components = mapOf(
+            0 to listOf("شح"),
+            1 to listOf("تمام"),
+            2 to listOf("عسل"),
+            3 to listOf("خادع"),
+            4 to listOf("حاسد"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 46,
+        components = mapOf(
+            0 to listOf("صد"),
+            1 to listOf("اضعف"),
+            2 to listOf("اول"),
+            3 to listOf("حمیم"),
+            4 to listOf("دعوی"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 47,
+        components = mapOf(
+            0 to listOf("صف"),
+            1 to listOf("فتاح"),
+            2 to listOf("حطب"),
+            3 to listOf("فارغ"),
+            4 to listOf("فاطر"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 48,
+        components = mapOf(
+            0 to listOf("ضر"),
+            1 to listOf("سافل"),
+            2 to listOf("بیع"),
+            3 to listOf("ساقط"),
+            4 to listOf("رفیق"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 49,
+        components = mapOf(
+            0 to listOf("ضد"),
+            1 to listOf("باخع"),
+            2 to listOf("جان"),
+            3 to listOf("فتنه"),
+            4 to listOf("دخان"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 50,
+        components = mapOf(
+            0 to listOf("صم"),
+            1 to listOf("بهجت"),
+            2 to listOf("خیر"),
+            3 to listOf("صادق"),
+            4 to listOf("مجید"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 51,
+        components = mapOf(
+            0 to listOf("شق"),
+            1 to listOf("قربی"),
+            2 to listOf("فوق"),
+            3 to listOf("قبله"),
+            4 to listOf("قبول"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 52,
+        components = mapOf(
+            0 to listOf("دع"),
+            1 to listOf("عربی"),
+            2 to listOf("جوع"),
+            3 to listOf("اقسط"),
+            4 to listOf("عبوس"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 53,
+        components = mapOf(
+            0 to listOf("رج"),
+            1 to listOf("سادت"),
+            2 to listOf("زاد"),
+            3 to listOf("رحلت"),
+            4 to listOf("جدال"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 54,
+        components = mapOf(
+            0 to listOf("ظل"),
+            1 to listOf("جدید"),
+            2 to listOf("حلی"),
+            3 to listOf("مطهر"),
+            4 to listOf("لیله"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 55,
+        components = mapOf(
+            0 to listOf("ظن"),
+            1 to listOf("نطفه"),
+            2 to listOf("یوم"),
+            3 to listOf("اوسط"),
+            4 to listOf("نفوس"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 56,
+        components = mapOf(
+            0 to listOf("عم"),
+            1 to listOf("واحد"),
+            2 to listOf("دین"),
+            3 to listOf("مجیب"),
+            4 to listOf("محیی"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 57,
+        components = mapOf(
+            0 to listOf("غد"),
+            1 to listOf("سرمد"),
+            2 to listOf("ضال"),
+            3 to listOf("سائغ"),
+            4 to listOf("دماء"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 58,
+        components = mapOf(
+            0 to listOf("غم"),
+            1 to listOf("وادی"),
+            2 to listOf("دین"),
+            3 to listOf("ادنی"),
+            4 to listOf("مدین"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 59,
+        components = mapOf(
+            0 to listOf("غل"),
+            1 to listOf("کرسی"),
+            2 to listOf("غار"),
+            3 to listOf("لعنت"),
+            4 to listOf("لسان"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 60,
+        components = mapOf(
+            0 to listOf("شق"),
+            1 to listOf("مداد"),
+            2 to listOf("اصل"),
+            3 to listOf("قرده"),
+            4 to listOf("قاصد"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 61,
+        components = mapOf(
+            0 to listOf("حج"),
+            1 to listOf("جبار"),
+            2 to listOf("اهل"),
+            3 to listOf("اذله"),
+            4 to listOf("جاهل"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 62,
+        components = mapOf(
+            0 to listOf("صب"),
+            1 to listOf("آلاء"),
+            2 to listOf("ذئب"),
+            3 to listOf("احسن"),
+            4 to listOf("بائس"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 63,
+        components = mapOf(
+            0 to listOf("صر"),
+            1 to listOf("خاشع"),
+            2 to listOf("ریح"),
+            3 to listOf("عقده"),
+            4 to listOf("رشید"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 64,
+        components = mapOf(
+            0 to listOf("طل"),
+            1 to listOf("مضطر"),
+            2 to listOf("عین"),
+            3 to listOf("کافر"),
+            4 to listOf("لطیف"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 65,
+        components = mapOf(
+            0 to listOf("عد"),
+            1 to listOf("زینت"),
+            2 to listOf("زید"),
+            3 to listOf("رماد"),
+            4 to listOf("دنیا"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 66,
+        components = mapOf(
+            0 to listOf("غی"),
+            1 to listOf("موتی"),
+            2 to listOf("سیر"),
+            3 to listOf("مزمل"),
+            4 to listOf("یتیم"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 67,
+        components = mapOf(
+            0 to listOf("فک"),
+            1 to listOf("کوثر"),
+            2 to listOf("سید"),
+            3 to listOf("مصرخ"),
+            4 to listOf("کثیر"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 68,
+        components = mapOf(
+            0 to listOf("فظ"),
+            1 to listOf("کاهن"),
+            2 to listOf("لین"),
+            3 to listOf("عارض"),
+            4 to listOf("ظهیر"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 69,
+        components = mapOf(
+            0 to listOf("کم"),
+            1 to listOf("مشرق"),
+            2 to listOf("صید"),
+            3 to listOf("رحمت"),
+            4 to listOf("مریم"),
+        )
+    ),
+    LevelComponentsTable(
+        id = 70,
+        components = mapOf(
+            0 to listOf("لد"),
+            1 to listOf("فارض"),
+            2 to listOf("اجل"),
+            3 to listOf("موهن"),
+            4 to listOf("درجه"),
+        )
+    )
+)
